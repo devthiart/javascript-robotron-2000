49c7751ccc0f70be021b9bf0ec55a2c4d5e676b7
devthiart <devthiart@gmail.com>
2023-02-28 16:15:41 -0300

    Class 05

--- js/main.js
+++ js/main.js
@@ -1,5 +1,39 @@
 const robotron = document.querySelector('#robotron');
 const controls = document.querySelectorAll('[data-control]');
+const statistics = document.querySelectorAll('[data-statistic]');
+const parts = {
+  "bracos": {
+      "forca": 29,
+      "poder": 35,
+      "energia": -21,
+      "velocidade": -5
+  },
+
+  "blindagem": {
+      "forca": 41,
+      "poder": 20,
+      "energia": 0,
+      "velocidade": -20
+  },
+  "nucleos":{
+      "forca": 0,
+      "poder": 7,
+      "energia": 48,
+      "velocidade": -24
+  },
+  "pernas":{
+      "forca": 27,
+      "poder": 21,
+      "energia": -32,
+      "velocidade": 42
+  },
+  "foguetes":{
+      "forca": 0,
+      "poder": 28,
+      "energia": 0,
+      "velocidade": -2
+  }
+}
 
 robotron.addEventListener('click', () => {
   console.log("Don't click me human!");
@@ -26,6 +60,13 @@ controls.forEach(
   (element) => {
     element.addEventListener('click', (event) => {
       manipulateData(event.target.dataset.control, event.target.parentNode);
+      updateStatistics(event.target.dataset.part);
     })
   }
 )
+
+function updateStatistics(part) {
+  statistics.forEach((element) => {
+    element.textContent = parseInt(element.textContent) + parts[part][element.dataset.statistic];
+  })
+}
